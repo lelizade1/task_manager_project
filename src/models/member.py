ab5e3f53ebf502d111ce9bc507c43ce32e15c806
zeynnalow <hzeynalov3@std.beu.edu.az>
2025-12-09 23:09:42 +0400

    Delete src/models/member.py

--- src/models/member.py
+++
@@ -1,7 +0,0 @@
-class TeamMember:
-    def __init__(self, name, role="Member"):
-        self.name = name
-        self.role = role
-
-    def __str__(self):
-        return f"{self.name} ({self.role})"
